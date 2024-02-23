22580e6880011b4b2562686eace4486686df2f7f
Sterenn <61694205+SterennLeHir@users.noreply.github.com>
2024-02-23 15:28:24 +0100

    Add writing file

--- app/src/main/java/esir/progmob/tp_android/TP2.kt
+++ app/src/main/java/esir/progmob/tp_android/TP2.kt
@@ -1,9 +1,13 @@
 package esir.progmob.tp_android
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 
 class TP2 : ComponentActivity() {
@@ -11,12 +15,23 @@ class TP2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tp2_layout1);
 
-        // Récupération du répertoire du stockage interne
-        val dir : File = this.getFilesDir();
+        val filename = "LEHIR-LECOMTE"
+        val fileContents = "Bonjour Sterenn ROUX"
+        this.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }
 
-        val file : File = File(dir, "fichier")
+        try {
+            val fileToRead : InputStream = openFileInput(filename)
+            val size : Int = fileToRead.available()
+            val buffer : ByteArray = ByteArray(size)
+            fileToRead.read(buffer)
+            fileToRead.close()
+            val content : String = String(buffer)
+        } catch(e : IOException) {
+            e.printStackTrace()
+        }
 
-        val fileToWrite : FileOutputStream = FileOutputStream(file)
 
 
 
